34ee436189e4528e6d8a6d40946a8948f27334ab
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2024-11-24 19:04:53 +0530

    changes added for memoisation problems

--- Problems/src/com/problems/recursion/Fibonacci.java
+++ Problems/src/com/problems/recursion/Fibonacci.java
@@ -1,13 +1,21 @@
 package com.problems.recursion;
 
+import java.util.Arrays;
+
 public class Fibonacci {
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 //			for(int i=1; i<=10;i++) {
 //				
 //				System.out.println(fibonacci(i));
 //			}
-			System.out.println(fibonacci(6));
-		}
+
+		int[] arr = new int[6 + 1];
+		Arrays.fill(arr, -1);
+		System.out.println(fibonacci(6));
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.print(arr[i] + " ");
+//		}
+	}
 
 		private static int fibonacci(int n) {
 			
@@ -18,4 +26,20 @@ public class Fibonacci {
 				
 			return fibonacci(n-1)+fibonacci(n-2);
 		}
+
+    // Memoization tech
+	private static int fibonacci(int n, int[] arr) {
+
+		if (arr[n] != -1) {
+			return arr[n];
+		}
+
+		if (n == 1 || n == 2) {
+			arr[n] = 1;
+			return arr[n];
+		}
+
+		arr[n] = fibonacci(n - 1, arr) + fibonacci(n - 2, arr);
+		return arr[n];
+	}
 }
