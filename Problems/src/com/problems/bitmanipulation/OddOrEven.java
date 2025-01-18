8b8aaa4732a4d5677ef570e314454098996d3e1e
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-01-18 13:03:04 +0530

    Even or odd with bitwise

---
+++ Problems/src/com/problems/bitmanipulation/OddOrEven.java
@@ -0,0 +1,9 @@
+package com.problems.bitmanipulation;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.println((num & 1) == 0);
+    }
+}
