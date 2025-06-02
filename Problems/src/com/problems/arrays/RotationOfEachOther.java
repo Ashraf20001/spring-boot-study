6e1b0cb68b846db9939579c84910dc997d59e08f
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-06-02 12:55:03 +0530

    rotation of each other logic changes

--- Problems/src/com/problems/arrays/RotationOfEachOther.java
+++ Problems/src/com/problems/arrays/RotationOfEachOther.java
@@ -1,38 +1,26 @@
 package com.problems.arrays;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class RotationOfEachOther {
     public static void main(String[] args) {
         int[] arr ={1,2,3,4,5} ; int[] arr2 = {3,4,5,1,2};
-
         System.out.println(isBothArrayRotationOfEachOther(arr,arr2));
     }
 
     private static boolean isBothArrayRotationOfEachOther(int[] arr, int[] arr2) {
-        List<Integer> list1 = Arrays.stream(arr).boxed().toList();
-        List<Integer> list2 =Arrays.stream(arr2).boxed().toList();
-        System.out.println(list1.get(0));
-        int i = list2.indexOf(list1.get(0));
-        if(i<0) return  false;
-
-        int count = 0;
-
-        for(int index = i; index< list2.size(); index ++){
-
-            if(index == count || list1.get(count) != list2.get(index)){
-                return  false;
-            }
-            count++;
+        StringBuilder array1 = new StringBuilder();
+        StringBuilder array2 = new StringBuilder();
+        for (int arr1: arr){
+            array1.append(arr1);
         }
 
-        for(int index =0; index < i;index++){
-            if(list1.get(count) != list2.get(index)){
-                return false;
-            }
+        for(int ar2 : arr2){
+            array2.append(ar2);
         }
 
-        return true;
+        String arr2String = new String(array2);
+        String joinedString = new String(array1.append(array1));
+
+        return  joinedString.contains(arr2String);
+
     }
 }
