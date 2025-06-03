6277f50265f4842e48488efcda5522b520d214a9
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-06-03 12:44:43 +0530

    binary search recursive approach added

--- Problems/src/com/problems/arrays/searching/alg/BinarySearch.java
+++ Problems/src/com/problems/arrays/searching/alg/BinarySearch.java
@@ -4,9 +4,9 @@ package com.problems.arrays.searching.alg;
 // Best for sorted arrays
 public class BinarySearch {
 		public static void main(String[] args) {
-			int k=15;
+			int k=20;
 			int[] arr = {2, 10, 14, 20};
-			int position = binarySearch(arr, k);
+			int position = binarySearch(arr, 0,arr.length-1,k);
 			System.out.println("position is "+position);
 		}
 
@@ -23,6 +23,22 @@ public class BinarySearch {
 				}
 				else{
 					l=mid-1;
+				}
+			}
+			return -1;
+	}
+
+	// recursive approach
+	private static int binarySearch(int[] arr,int l,int h,int k){
+			if(l<=h){
+				int m = (l+h)/2;
+				if(arr[m] == k){
+					return m;
+				}
+				else if(arr[m]<k){
+					return binarySearch(arr,m+1,h,k);
+				}else{
+					return binarySearch(arr,l,m-1,k);
 				}
 			}
 			return -1;
