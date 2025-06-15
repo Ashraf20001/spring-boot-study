ea743e098a680f12d8b5a2dee3d534094b6d5de7
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-06-15 22:10:55 +0530

    maximum distinct sub array sum of length k

--- Problems/src/com/problems/arrays/SlidingWindowAlg.java
+++ Problems/src/com/problems/arrays/SlidingWindowAlg.java
@@ -1,5 +1,9 @@
 package com.problems.arrays;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class SlidingWindowAlg {
     public static void main(String[] args) {
         int[] arr={2,9,31,-4,21,7};
@@ -18,5 +22,89 @@ public class SlidingWindowAlg {
             maxSum=Math.max(wSum,maxSum);
         }
         return maxSum;
+    }
+
+    private static long findMaximumDistinctSubArrayOfLengthK(int[] arr, int k) {
+        long currentSum = 0;
+        long maxSum = 0;
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        // Fixed left initially at start 0
+        int l = 0;
+        for(int r=0; r<arr.length;r++){
+            // Window creating
+            currentSum+=arr[r];
+            frequencyMap.put(arr[r], frequencyMap.getOrDefault(arr[r],0)+1);
+
+            while(frequencyMap.get(arr[r])>1 || (r-l+1) >k ){
+                currentSum-=arr[l];
+                frequencyMap.put(arr[l], frequencyMap.get(arr[l]) - 1);
+                if (frequencyMap.get(arr[l]) == 0) {
+                    frequencyMap.remove(arr[l]);
+                }
+                l++;
+            }
+
+            if(r-l+1 == k){
+                maxSum = Math.max(currentSum,maxSum);
+            }
+        }
+        return maxSum;
+
+        /*
+             HashSet<Integer> set = new HashSet<>();
+        long sum = 0;
+        long maxSum = 0;
+        int i = 0;
+
+        for (int j = 0; j < arr.length; j++) {
+            while (set.contains(arr[j])) {
+                set.remove(arr[i]);
+                sum -= arr[i];
+                i++;
+            }
+
+            set.add(arr[j]);
+            sum += arr[j];
+
+            if (j - i + 1 == k) {
+                maxSum = Math.max(maxSum, sum);
+                sum -= arr[i];
+                set.remove(arr[i]);
+                i++;
+            }
+        }
+
+        return maxSum;
+         */
+
+        /*
+
+             Set<Integer> set = new HashSet<>();
+        long maxSum = 0, windowSum = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while (set.contains(nums[right])) {
+                // Remove leftmost element until we can add nums[right]
+                set.remove(nums[left]);
+                windowSum -= nums[left];
+                left++;
+            }
+
+            set.add(nums[right]);
+            windowSum += nums[right];
+
+            // If window size is exactly k
+            if (right - left + 1 == k) {
+                maxSum = Math.max(maxSum, windowSum);
+                // Slide window forward
+                set.remove(nums[left]);
+                windowSum -= nums[left];
+                left++;
+            }
+        }
+
+        return maxSum;
+         */
     }
 }
