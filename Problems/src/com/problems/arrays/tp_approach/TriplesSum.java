09d1d3b4994d3517a161d8d4ac2f157af6c53a17
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-06-27 13:08:08 +0530

    Triple sum efficient algorithm changes

--- Problems/src/com/problems/arrays/tp_approach/TriplesSum.java
+++ Problems/src/com/problems/arrays/tp_approach/TriplesSum.java
@@ -2,51 +2,43 @@ package com.problems.arrays.tp_approach;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class TriplesSum {
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};  // -4,-1,-1,-1,2,2
+        int[] nums = {2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10};  // -4,-1,-1,-1,2,2
         System.out.println(threeSum(nums));
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
 
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // skip duplicates by checking with previous element
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            int l = i+1;
-            int h = nums.length - 1;
-            while (l < h) {
-                List<Integer> ans = new ArrayList<Integer>();
-                if (nums[l] + nums[h] + nums[i] == 0) {
-                    ans.add(nums[l]);
-                    ans.add(nums[h]);
-                    ans.add(nums[i]);
-                    res.add(ans);
-                    if(l<nums.length-1 && nums[l]==nums[l+1]){
-                        l++;
-                    } if(h>0 && nums[h]==nums[h-1]){
-                        h--;
-                    }
-                l++;
-                 h--;
-                } else if (nums[l] + nums[h] < 0) {
-                    l++;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
                 } else {
-                    h--;
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
                 }
             }
-
         }
-
         return res;
     }
 }
