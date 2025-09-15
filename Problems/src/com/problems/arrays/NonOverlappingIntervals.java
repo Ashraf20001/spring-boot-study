27f4fd02b6a884850a6fa38cf26e7bdda132328e
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-09-15 12:59:46 +0530

    New changes added

--- Problems/src/com/problems/arrays/NonOverlappingIntervals.java
+++ Problems/src/com/problems/arrays/NonOverlappingIntervals.java
@@ -1,31 +1,28 @@
 package com.problems.arrays;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class NonOverlappingIntervals {
     public static void main(String[] args) {
-        int[][] intervals = {{1,2},{2,3},{3,4},{1,3}};
+        int[][] intervals = {{1,2},{2,3}};
         int nonOverlappedIntervals = getNonOverlappingIntervalCount(intervals);
         System.out.println(nonOverlappedIntervals);
     }
 
     private static int getNonOverlappingIntervalCount(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
-        List<int[]> intervalsList = new ArrayList<>();
-        intervalsList.add(intervals[0]);
-        int overlappingCount = 0;
-        for (int i=1; i<intervals.length ; i++){
-
-                if(intervalsList.getLast()[1]<=intervals[i][0]){
-                    intervalsList.add(intervals[i]);
-                }else {
-                    overlappingCount++;
-                }
+        int previousElement = intervals[0][1];
+        int count = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (previousElement <= intervals[i][0]) {
+                previousElement = intervals[i][1];
+            } else {
+                count++;
             }
-
-        return overlappingCount;
         }
+
+        return count;
+
+    }
 }
