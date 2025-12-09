094836d4b60def452afa8b5c6a39d6691f07e4b8
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-12-09 17:54:55 +0530

    Sliding window for average calculation

---
+++ Problems/src/com/problems/stream_problems/AverageSlidingWindow.java
@@ -0,0 +1,28 @@
+package com.problems.stream_problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class AverageSlidingWindow {
+    public static void main(String[] args) {
+        List<Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(1);
+        integerArrayList.add(14);
+        integerArrayList.add(21);
+        integerArrayList.add(10);
+        integerArrayList.add(61);
+
+        int window = 2;
+
+        List<Integer> list = IntStream.range(0, integerArrayList.size() - window + 1)
+                .mapToObj(intElement -> integerArrayList.get(intElement) + integerArrayList.get(intElement + 1))
+                .toList();
+        System.out.println(list);
+        System.out.println(Stream.iterate(0,el->el+1).limit(integerArrayList.size()-window+1)
+                        .map(i->integerArrayList.get(i)+integerArrayList.get(i+1))
+                        .toList());
+
+    }
+}
