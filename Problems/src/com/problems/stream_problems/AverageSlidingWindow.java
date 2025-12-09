48b0f45e89d2203dab418e24bc9422f468bcdb23
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-12-09 22:36:54 +0530

    Sliding window for average calculation changes

--- Problems/src/com/problems/stream_problems/AverageSlidingWindow.java
+++ Problems/src/com/problems/stream_problems/AverageSlidingWindow.java
@@ -2,6 +2,7 @@ package com.problems.stream_problems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -17,12 +18,21 @@ public class AverageSlidingWindow {
         int window = 2;
 
         List<Integer> list = IntStream.range(0, integerArrayList.size() - window + 1)
-                .mapToObj(intElement -> integerArrayList.get(intElement) + integerArrayList.get(intElement + 1))
+                .mapToObj(intElement -> (integerArrayList.get(intElement) + integerArrayList.get(intElement + 1))/window)
                 .toList();
         System.out.println(list);
         System.out.println(Stream.iterate(0,el->el+1).limit(integerArrayList.size()-window+1)
-                        .map(i->integerArrayList.get(i)+integerArrayList.get(i+1))
+                        .map(i->(integerArrayList.get(i)+integerArrayList.get(i+1))/window)
                         .toList());
+
+        // Generic approach
+
+        System.out.println(IntStream.range(0, integerArrayList.size() - window + 1)
+                .mapToObj(i->integerArrayList.subList(i,i+window))
+                .map(el->el.stream().mapToInt(Integer::intValue).average())
+                .map(OptionalDouble::getAsDouble)
+                .toList());
+
 
     }
 }
