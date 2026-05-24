ee40ab1d6f0d06cfb217d362b4faf97ed9a1216f
Ashraf20001 <ashrafbsz001@gmail.com>
2026-05-24 19:51:18 +0530

    Time complexity of trailing zeroes.

--- Problems/src/com/problems/java/TrailingZeroes.java
+++ Problems/src/com/problems/java/TrailingZeroes.java
@@ -19,9 +19,23 @@ public class TrailingZeroes {
 		while(userInputInteger>=powerOf5) {
 			int ans=userInputInteger/powerOf5;
 			res+=ans;
-			powerOf5*=powerOf5;
+			powerOf5=5 * powerOf5;
 		}
 		return res;
 	}
+
+    /**
+     * Time complexity:
+     * N is the input integer
+     * (5)1 = 0 iteration
+     * (5)1(2) = 1 iteration = (5)2
+     * (5)2(1) = 2 iterations = (5)3
+     * (5)2(2) = 3 iterations = 5(4)
+     *
+     * (5)2k <= N
+     * 2^k <= log5N
+     * k <= log2(log5N) O(loglogN)
+      */
+
 
 }
